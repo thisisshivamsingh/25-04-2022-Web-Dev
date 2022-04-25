50eec5ca52df91d28fe723d7a477b9cb0b7162ee
thisisshivamsingh <96998286+thisisshivamsingh@users.noreply.github.com>
2022-04-25 13:53:18 +0530

    Delete childprocess.js

--- childprocess.js
+++
@@ -1,8 +0,0 @@
-let cp = require("child_process");
-console.log("Trying to open our ❤");
-// cp.execSync("calc");
-// cp.execSync("code")
-// cp.execSync("start chrome https:\\www.pepcoding.com");
-console.log("opened our ❤");
-let output = cp.execSync("node abc.js");
-console.log("output 🔥 " + output);
